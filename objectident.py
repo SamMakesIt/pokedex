e884325684f0e345d86ee09b83f1c695d3d7c590
sampaladino <sampaladino88@gmail.com>
2022-02-06 10:36:41 -0600

    added comments

--- objectident.py
+++ objectident.py
@@ -1,30 +1,34 @@
+## object identification module 
 import cv2
+##Lets tts say numbers
 from num2words import num2words
+##subprocess opens a terminal to run tts commands 
 from subprocess import call
 
 #thres = 0.45 # Threshold to detect object
 
-cmd_beg= 'sudo espeak '
-cmd_end= ' 2>/dev/null'
-cmd_voice= '-ven+f4 '
-classNames = []
+cmd_beg= 'sudo espeak '             ## puts sudo espeak in term
+cmd_end= ' 2>/dev/null'                ## cleans up the output from the terminal
+cmd_voice= '-ven+f4 '               ## Assigns which voice ill be using
+classNames = []                     ## coco.name
 #call([cmd_beg+text+cmd_end] shell=True)
-cv2Text = cv2.putText
-cv2.putText = classNames[classId - 1].upper()
-classNames = classId
-classFile = "/home/pi/Desktop/pokedex/coco.names"
-with open(classFile,"rt") as f:
+cv2Text = cv2.putText                           ##
+cv2.putText = classNames[classId - 1].upper()  ## not sure if this is right tbth
+classNames = classId                            ##
+classFile = "/home/pi/Desktop/pokedex/coco.names"  ## tells script where names are stored
+with open(classFile,"rt") as f:                        
     classNames = f.read().rstrip("\n").split("\n")
-configPath = "/home/pi/Desktop/pokedex/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt"
+configPath = "/home/pi/Desktop/pokedex/ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt" #These 2 lines are the object detection DBs
 weightsPath = "/home/pi/Desktop/pokedex/frozen_inference_graph.pb"
 
+## this is more standard detection stuff
 net = cv2.dnn_DetectionModel(weightsPath,configPath)
 net.setInputSize(320,320)
 net.setInputScale(1.0/ 127.5)
 net.setInputMean((127.5, 127.5, 127.5))
 net.setInputSwapRB(True)
 
-
+##this is the meat of the detection, tells it how to draw the box, and to label inside the box
 def getObjects(img, thres, nms, draw=True, objects=[]):
     classIds, confs, bbox = net.detect(img,confThreshold=thres,nmsThreshold=nms)
     #print(classIds,bbox)
@@ -46,12 +50,15 @@ def getObjects(img, thres, nms, draw=True, objects=[]):
 
     return img,objectInfo
 
+## this is the test function for TTS im lost here tbth
+def tts():
+    if cv2Text == 'DOG':
+        call([cmd_beg+cmd_voice+" I+am+a+dog"+cmd_end], shell=True)
+    if cv2Text == 'PERSON':
+        call([cmd_beg+cmd_voice+" I+am+a+person"+cmd_end], shell=True)
 
-if cv2Text == 'DOG':
-    call([cmd_beg+cmd_voice+" I+am+a+dog"+cmd_end], shell=True)
-if cv2Text == 'PERSON':
-    call([cmd_beg+cmd_voice+" I+am+a+person"+cmd_end], shell=True)
 
+## this is getting the video feed
 if __name__ == "__main__":
 
     cap = cv2.VideoCapture(0)
@@ -59,7 +66,7 @@ if __name__ == "__main__":
     cap.set(4,480)
     #cap.set(10,70)
 
-
+## this is showing me the output on screen
     while True:
             success, img = cap.read()
             result, objectInfo = getObjects(img,0.65,0.6, objects=['dog','person']), tts
